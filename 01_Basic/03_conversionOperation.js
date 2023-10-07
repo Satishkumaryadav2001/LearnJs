3a7b3a843c486a8c9e05c3a93af889942be1fe89
Satish Kumar Yadav <112901397+SatishKumarYadav7518381304@users.noreply.github.com>
2023-10-07 10:21:39 +0000

    Conversion

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,70 +1,117 @@
-let score=33;
+// let score=33;
 
-console.log(typeof score);//number
-
-
-
-let score1="33avc";
+// console.log(typeof score);//number
 
 
-console.log(typeof score1);//string
+
+// let score1="33avc";
 
 
-let  valueInNumber = Number(score1);
-
-console.log(typeof score1);//string
-console.log(typeof valueInNumber);//number
-console.log(valueInNumber);//NaN
-
-let score2=null;
-
-console.log(typeof score2);//object
+// console.log(typeof score1);//string
 
 
-let  valueInNumber1 = Number(score1);
+// let  valueInNumber = Number(score1);
 
-console.log(typeof score2);//object
-console.log(typeof valueInNumber1);//number
-console.log(valueInNumber1);//NaN
+// console.log(typeof score1);//string
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//NaN
 
-let score3=undefined;
+// let score2=null;
 
-console.log(typeof score3);//undefined
-
-
-let  valueInNumber2 = Number(score1);
-
-console.log(typeof score3);//undefined
-console.log(typeof valueInNumber2);//number
-console.log(valueInNumber2);//NaN
-
-let score4=true;
-
-console.log(typeof score4);//boolean
+// console.log(typeof score2);//object
 
 
-let  valueInNumber3 = Number(score1);
+// let  valueInNumber1 = Number(score1);
 
-console.log(typeof score4);//boolean
-console.log(typeof valueInNumber3);//number
-console.log(valueInNumber3);//1
+// console.log(typeof score2);//object
+// console.log(typeof valueInNumber1);//number
+// console.log(valueInNumber1);//NaN
 
+// let score3=undefined;
 
-//"33" =>33
-//"33ab" =>NaN
-//true => 1; false => 0
-
-
-let isLoggedIn="jh"
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);//true
-
-//1 => true; 0 => false
-//"" =>false
-//"hi" =>true
+// console.log(typeof score3);//undefined
 
 
-let someNumber=33
-let stringNumber=String(someNumber);
-console.log(stringNumber);//33
-console.log(typeof stringNumber);//string
+// let  valueInNumber2 = Number(score1);
+
+// console.log(typeof score3);//undefined
+// console.log(typeof valueInNumber2);//number
+// console.log(valueInNumber2);//NaN
+
+// let score4=true;
+
+// console.log(typeof score4);//boolean
+
+
+// let  valueInNumber3 = Number(score1);
+
+// console.log(typeof score4);//boolean
+// console.log(typeof valueInNumber3);//number
+// console.log(valueInNumber3);//1
+
+
+// //"33" =>33
+// //"33ab" =>NaN
+// //true => 1; false => 0
+
+
+// let isLoggedIn="jh"
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);//true
+
+// //1 => true; 0 => false
+// //"" =>false
+// //"hi" =>true
+
+
+// let someNumber=33
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);//33
+// console.log(typeof stringNumber);//string
+
+
+
+//******************************************Oprations************************************** */
+
+let value=3
+let negValue=-value
+console.log(value);//3
+console.log(negValue);//-3
+
+console.log(2+3);//5
+console.log(2-3);//-1
+console.log(2*3);//6
+console.log(2/3);//0.66666666666666
+console.log(2%3);//2
+
+let str1="hello";
+let str2 ="Satish";
+
+let str3=str1 + str2;
+console.log(str3);//HelloSatish
+
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log("1"+"2");//12
+console.log("1"+2+2);//122
+console.log(1 + 2 + "2");//32
+
+console.log((100+2)/5);//20.4
+
+console.log(true)//true
+console.log(+true)//1
+// console.log(true+);//Unexpected
+console.log(+"")//0
+
+let num1, num2,num3
+
+num1=mum2=num3
+
+let gameCounter=100;
+console.log(gameCounter++);//100
+console.log(++gameCounter);//102
+
+//link to study
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
+
