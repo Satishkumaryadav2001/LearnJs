2b3eb7372f4283c0b4757b9d03899df90800833b
Satish Kumar Yadav <112901397+SatishKumarYadav7518381304@users.noreply.github.com>
2023-10-14 02:03:45 +0000

    object

---
+++ 02_Basic/03_object.js
@@ -0,0 +1,16 @@
+//singleton   -> Object.create
+
+//object literals   
+
+const jsUser={           //Object
+
+      name:"Satish",
+      age:23,
+      email:"satish@google.com",
+      location:"Sonbhadra",
+      isLoggedIn:false,
+      iastLoginDays:["Monday","Saturday"]
+
+}
+console.log(jsUser.email);
+console.log(jsUser[email]);
