5069beda4e47cb87dcb478f9be7856ce043b20a7
Satish Kumar Yadav <112901397+SatishKumarYadav7518381304@users.noreply.github.com>
2023-10-18 10:25:24 +0000

    control_flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,78 @@
+//  const UserEmail="Satish@google.com";
+
+
+// if (UserEmail) {
+//     console.log("Got user Email");//Got user Email
+// }
+// else{
+//     console.log("Don't have user email");
+// }
+
+
+// const UserEmail="";
+
+// if (UserEmail) {
+//     console.log("Got user Email");
+// }
+// else{
+//     console.log("Don't have user email");//Don't have user email
+// }
+
+// const UserEmail= [];
+
+// if (UserEmail) {
+//     console.log("Got user Email");//Got user Email
+// }
+// else{
+//     console.log("Don't have user email");//Don't have user email
+// }
+
+
+// falsy values
+
+// false , 0,-0,BigInt,0n,"",null,undefined,NaN
+
+// Truty Value
+
+// true,"0",'false'," ",[],[],function(){}
+
+// const UserEmail="";
+// if (UserEmail.length===0) {
+// console.log("Array is Empty");//Array is Empty
+    
+// }
+
+// const emptyObj={}
+// if (Object.keys(emptyObj).length===0) {
+//     console.log("Object is Empty");//Object is Empty
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5  ?? 10
+// console.log(val1);//5
+
+// let val2;
+
+// val2 =null ?? 10
+// console.log(val2);//10
+
+// let val3;
+// val3 = undefined ?? 15
+// console.log(val3);//15
+
+
+// let val4;
+// val4=null ?? 3 ?? 4
+// console.log(val4);//3
+
+
+// Terniary Oprator
+
+// Condition ? true : false
+
+// const iceTeaPrice=100;
+// iceTeaPrice <=80 ? console.log("less than 80") : console.log("More than 80") ; //More than 80
+
